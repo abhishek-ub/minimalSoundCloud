ca958328ff75a508b8533b659438af30b4bec55e
abhishek-ub <a35@buffalo.edu>
2016-01-25 13:32:15 -0500

    search test

--- js/app.js
+++ js/app.js
@@ -2,20 +2,31 @@
 function login () {
     SC.initialize({
           client_id: '14bf255b6a8682914f41bdc90e3d4bde',
-      redirect_uri: 'http://localhost/soundcloud-callback.html'
+      redirect_uri: 'http://minimalsoundcloud.bitballoon.com/soundcloud-callback.html'
     });
 
     // initiate auth popup
     SC.connect().then(function() {
       return SC.get('/me');
     }).then(function(me) {
-         alert('Hello, ' + me.username);
+         console.log('Hello, ' + me.username);
+         removelogon();
+    });
+
+    SC.get('/tracks', {
+          q: 'buskers', license: 'cc-by-sa'
+    }).then(function(tracks) {
+          console.log(tracks);
     });
 }
 
 
+function removelogon() {
+    $("#loginbutton").remove();
+}
+
+
 $(function () {
-    alert("hey");
     $("#loginbutton").click( function() {
         login();
     });
